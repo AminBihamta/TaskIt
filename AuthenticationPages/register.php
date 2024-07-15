478bba54b7d8ac8e837544e20dc2dd83dc62c39e
Radowan Ahmed Baized <93855186+Radowan30@users.noreply.github.com>
2024-07-15 21:58:17 +0800

    User type is added upon registering

--- AuthenticationPages/register.php
+++ AuthenticationPages/register.php
@@ -7,28 +7,22 @@ require_once ("../config.php");
 $nickname = trim($_POST["NickName"]);
 $email = trim($_POST["Email"]);
 $password = trim($_POST["Password"]);
+$type = "User";
 
 
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO user (nickname, email, password) VALUES (?, ?, ?)";
+$sql = "INSERT INTO user (nickname, email, password, UserType) VALUES (?, ?, ?, ?)";
 
 
 if ($stmt = mysqli_prepare($conn, $sql)) {
     // Bind variables to the prepared statement as parameters
-    mysqli_stmt_bind_param($stmt, "sss", $nickname, $email, $hashedPassword);
+    mysqli_stmt_bind_param($stmt, "ssss", $nickname, $email, $hashedPassword, $type);
 
     // Attempt to execute the prepared statement
     if (mysqli_stmt_execute($stmt)) {
         $_SESSION['$userEmail'] = $email;
         $_SESSION['$userNickname'] = $nickname;
-
-        $sqlUpdate = "UPDATE user SET UserType = 'user' WHERE email = ?";
-        $stmtUpdate = $conn->prepare($sqlUpdate);
-        $stmtUpdate->bind_param("s", $email);
-        $stmtUpdate->execute();
-
-
         header("Location:../AppPages/dashboard.php");
         exit();
     } else {
